2e435226289107be1310125fda65367653780340
Phan Phát Tài <taib2014946@student.ctu.edu.vn>
2023-03-28 12:39:23 +0700

    Update heapSort.c

--- Buoi1/heapSort.c
+++ Buoi1/heapSort.c
@@ -2,7 +2,6 @@
 // Created by TaiTitans on 3/28/2023.
 //Heap Sort (Vun dong)
 //
-
 #include <stdio.h>
 
 typedef int keytype;
@@ -19,8 +18,36 @@ void swap(recordtype *x, recordtype *y){
     *x = *y;
     *y = t;
 }
-
-
+void PushDown(recordtype a[], int first, int last){
+    int r = first;
+    while( r <= (last-1)/2)
+        if(last == 2*r+1){
+            if(a[r].key > a[last].key) swap (&a[r], &a[last]);
+            r = last;
+        }else
+        if( (a[r].key > a[2*r+1].key) && (a[2*r+1].key <= a[2*r+2].key)){
+            swap(&a[r], &a[2*r+1]);
+            r = 2*r+1;
+        }else
+        if( (a[r].key > a[2*r+2].key) && (a[2*r+2].key < a[2*r+1].key)){
+            swap(&a[r], &a[2*r+2]);
+            r = 2*r+2;
+        }
+        else
+            r = last;
+}
+void heapSort(recordtype a[], int n){
+    int i;
+    //tao heap
+    for(i = (n-2)/2;i>=0;i--){
+        PushDown(a,i,n-1);
+    }
+    for(i=n-1;i>=2;i--){
+        swap(&a[0], &a[i]);
+        PushDown(a,0,i-1);
+    }
+    swap(&a[0], &a[1]);
+}
 void read_Data(recordtype a[], int *n){
  FILE *f;
  f= fopen("C:\\Users\\taiti\\OneDrive - ctu.edu.vn\\Documents\\GitHub\\CT174_PTTKTT\\CT174_PTTKTT_Algorithm-Analysis-And-Design\\Buoi1\\data.txt","r");
